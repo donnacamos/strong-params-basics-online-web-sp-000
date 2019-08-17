32c727077d1c44e7adca110dfe36106807d72acb
Donna Amos <donnacamos.developer@gmail.com>
2019-08-17 18:02:17 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 
 	private
 
-	def post_params(*args)
-    params.require(:post).permit(*args)
-  end
+	def post_params
+  params.require(:post).permit(:title, :description)
+end
 end
