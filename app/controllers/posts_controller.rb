476692a1cd0de1af3045fce508b64c6b487cd3d1
Donna Amos <donnacamos.developer@gmail.com>
2019-08-17 17:48:39 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,7 +27,8 @@ class PostsController < ApplicationController
 
 	def post_params
 		params.require(:post).permit(:title, :description)
-
+  end 
+	
 	def edit
 	  @post = Post.find(params[:id])
 	end
